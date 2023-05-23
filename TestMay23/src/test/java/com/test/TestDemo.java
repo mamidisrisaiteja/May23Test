3261feba5f4840c6c6bbc5088537d442b5f77c05
saiteja <mamidisrisaiteja@gmail.com>
2023-05-23 13:54:37 +0530

    initialCommit

---
+++ TestMay23/src/test/java/com/test/TestDemo.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	
+	@Test
+	public void testRun() {
+		Assert.assertTrue(true);
+		
+	}
+
+}
